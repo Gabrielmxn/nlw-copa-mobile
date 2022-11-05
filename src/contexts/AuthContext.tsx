1f1595b6b02c29b4b89a505a837e13388e83214a
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-11-04 23:09:22 -0300

    :sparkles: Adiciona autênticação com o google. Além disso, colocar no headers do axios o token hash e pega as informações do usuário

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -46,7 +47,21 @@ export function AuthContextProvider({ children }: AuthProviderProps){
   }
 
   async function signInWithGoogle(accessToken: string){
-    console.log(`Token de autenticção ${accessToken}`)
+    try{
+      const { data } = await api.post('/users', {
+        access_token: accessToken
+      })
+      console.log(data)
+      api.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
+      console.log(`Bearer ${data.token}`)
+      const userInfoReponse = await api.get('/me')
+      setUser(userInfoReponse.data.user)
+    } catch (error){
+      console.log(error)
+      throw error;
+    } finally{
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
@@ -59,10 +74,7 @@ export function AuthContextProvider({ children }: AuthProviderProps){
     <AuthContext.Provider value={{
       signIn,
       isUserLoading,
-      user:{
-        name: 'Gabriel',
-        avatarUrl: "https://github.com/gabrielmxn.png"
-      }
+      user,
     }}>
       {children}
     </AuthContext.Provider>
