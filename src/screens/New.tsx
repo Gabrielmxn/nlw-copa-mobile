1930b9c30bc52cbe1489238186b2a8898a5b4092
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-11-04 23:11:31 -0300

    :sparkles: Adiciona a criação de um novo bolão

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -2,10 +2,24 @@ import { Heading, Text, VStack } from "native-base";
 import { Header } from "../components/Header";
 
 import Logo from '../assets/logo.svg';
+import { useState } from 'react';
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { api } from "../services/api";
 
 export function New(){
+  const [namePoll, setNamePool] = useState('')
+
+  function handleNamePool(text: string){
+    setNamePool(text)
+  }
+  async function newCreatePool(){
+   const { data } = await api.post('pools', {
+    title: namePoll
+   })
+
+   setNamePool('');
+  }
   return(
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -18,10 +32,15 @@ export function New(){
         </Heading>
 
         <Input
-          mb={2} 
+          mb={2}
           placeholder="Qual nome do seu bolão?"
+          value={namePoll}
+          onChangeText={handleNamePool}
         />
-        <Button title="CRIAR MEU BALÃO" />
+        <Button 
+          title="CRIAR MEU BALÃO"
+          onPress={newCreatePool} 
+        />
         <Text textAlign="center" color="gray.200" fontSize="sm" px={10} mt={4}>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas.</Text>
       </VStack>
       
