abc48963a5acd388b51c39d6e5f325f42ba932bb
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-11-05 17:43:45 -0300

    :sparkles: Lista todos os bolões

--- src/screens/MyPool.tsx
+++ src/screens/MyPool.tsx
@@ -1,28 +1,56 @@
-import { useEffect } from 'react';
+import { useState, useCallback } from 'react';
 
-import { Box, Heading, Icon, Text, VStack } from "native-base";
+import { Box, FlatList, Heading, Icon, Text, ScrollView, VStack, Flex, useToast } from "native-base";
 import { Header } from "../components/Header";
 import { MagnifyingGlass } from 'phosphor-react-native'
 import Logo from '../assets/logo.svg';
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
-
+import { Loading } from "../components/Loading";
 import { ButtonIcon } from "../components/ButtonIcon";
 import { EmptyPoolList } from "../components/EmptyPoolList";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { api } from '../services/api';
+import { PoolCard, PoolPros } from '../components/PoolCard';
 
-export function Pools(){
+interface PoolsProps {
+  pools: PoolPros[];
+}
+
+interface PoolsPropsState {
+  data: PoolPros[];
+}
+export function Pools(props){
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState([] as PoolPros[])
   const navigation = useNavigation();
-  useEffect(() => {
-    async function getPools() {
-      const { data} = await api.get('/pools');
+  const toast = useToast();
 
-      console.log(data)
+  useFocusEffect(useCallback(() => {
+   try{
+    async function getPools() {
+      const { data } = await api.get<PoolsProps>('/pools');
+
+      console.log(data.pools)
+      setPools(data.pools)
     }
     getPools();
-  }, [])
+   } catch (err) {
+    console.log(err);
+
+    toast.show({
+      title: "Não foi possível carregar os bolões",
+      placement: 'top',
+      bgColor: 'red.500'
+    })
+   } finally {
+    setIsLoading(false)
+   }
+
+    
+  }, [props]))
+
 
   return(
     <VStack flex={1} bgColor="gray.900">
@@ -39,11 +67,23 @@ export function Pools(){
         
        
       </VStack>
-        <Box px={3}>
-          <EmptyPoolList 
+        
+         {
+            isLoading ?  <Flex justifyContent="center"  alignItems="center"borderWidth="1" borderColor="red">
+            <Loading />
+           </Flex>  : <FlatList 
+          data={pools}
+          keyExtractor={item => item.id}
+          renderItem={( { item} ) => <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{pb: 20}}
 
-          />
-        </Box>
+          ListEmptyComponent={( ) => <EmptyPoolList />}
+        />
+         }
+        
+          
         
     </VStack>
   )
